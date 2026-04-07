ca0e3c5e67b90d524d8d619f5695a0cb90da4378
Zero3Nine <67416696+zero3nine@users.noreply.github.com>
2026-04-07 21:08:38 +0530

    fix(auth): db migration update fix

--- backend/AuthService/Auth.Infrastructure/Data/Migrations/20260407152548_InitialCreate.Designer.cs
+++ backend/AuthService/Auth.Infrastructure/Data/Migrations/20260407152548_InitialCreate.Designer.cs
@@ -12,7 +12,7 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Auth.Infrastructure.Data.Migrations
 {
     [DbContext(typeof(AuthDbContext))]
-    [Migration("20260407151734_InitialCreate")]
+    [Migration("20260407152548_InitialCreate")]
     partial class InitialCreate
     {
         /// <inheritdoc />
