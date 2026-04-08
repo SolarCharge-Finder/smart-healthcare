8ae9ae5606686d3d90f9d93b75d4068b543cf77b
Zero3Nine <67416696+zero3nine@users.noreply.github.com>
2026-04-08 17:29:24 +0530

    chore(auth): migration update

--- backend/AuthService/Auth.Infrastructure/Data/Migrations/AuthDbContextModelSnapshot.cs
+++ backend/AuthService/Auth.Infrastructure/Data/Migrations/AuthDbContextModelSnapshot.cs
@@ -22,6 +22,44 @@ namespace Auth.Infrastructure.Data.Migrations
 
             NpgsqlModelBuilderExtensions.UseIdentityByDefaultColumns(modelBuilder);
 
+            modelBuilder.Entity("Auth.Domain.Entities.PendingUser", b =>
+                {
+                    b.Property<Guid>("Id")
+                        .ValueGeneratedOnAdd()
+                        .HasColumnType("uuid");
+
+                    b.Property<string>("Email")
+                        .IsRequired()
+                        .HasColumnType("text");
+
+                    b.Property<DateTime>("ExpiresAt")
+                        .HasColumnType("timestamp with time zone");
+
+                    b.Property<string>("Name")
+                        .IsRequired()
+                        .HasColumnType("text");
+
+                    b.Property<string>("PasswordHash")
+                        .IsRequired()
+                        .HasColumnType("text");
+
+                    b.Property<int>("Role")
+                        .HasColumnType("integer");
+
+                    b.Property<string>("VerificationToken")
+                        .IsRequired()
+                        .HasColumnType("text");
+
+                    b.HasKey("Id");
+
+                    b.HasIndex("Email");
+
+                    b.HasIndex("VerificationToken")
+                        .IsUnique();
+
+                    b.ToTable("PendingUsers");
+                });
+
             modelBuilder.Entity("Auth.Domain.Entities.User", b =>
                 {
                     b.Property<Guid>("Id")
