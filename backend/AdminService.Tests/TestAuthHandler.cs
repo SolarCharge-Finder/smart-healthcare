3a8fe776e17dff5e0106b2e55d3942a8b6e2842d
Zero3Nine <67416696+zero3nine@users.noreply.github.com>
2026-04-13 21:47:59 +0530

    chore: deprecated tests functionality fix

--- backend/AdminService.Tests/TestAuthHandler.cs
+++ backend/AdminService.Tests/TestAuthHandler.cs
@@ -12,9 +12,8 @@ public class TestAuthHandler : AuthenticationHandler<AuthenticationSchemeOptions
     public TestAuthHandler(
         IOptionsMonitor<AuthenticationSchemeOptions> options,
         ILoggerFactory logger,
-        UrlEncoder encoder,
-        ISystemClock clock)
-        : base(options, logger, encoder, clock)
+        UrlEncoder encoder)
+        : base(options, logger, encoder)
     {
     }
 
