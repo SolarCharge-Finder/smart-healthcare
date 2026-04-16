8df8c8b22c84e54ddf8a6a9b88790b06585512bc
Zero3Nine <67416696+zero3nine@users.noreply.github.com>
2026-04-16 15:39:13 +0530

    style: backend formatting changes

--- backend/DoctorService/Doctor.API/Controllers/DoctorController.cs
+++ backend/DoctorService/Doctor.API/Controllers/DoctorController.cs
@@ -57,16 +57,22 @@ public class DoctorController : ControllerBase
 
         // fallback if you used NameIdentifier instead
         if (userId == null)
+        {
             userId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+        }
 
         if (userId == null)
+        {
             return Unauthorized("User ID not found in token.");
+        }
 
         // get doctor by userId
         var doctor = await _service.GetByUserId(Guid.Parse(userId));
 
         if (doctor == null)
+        {
             return NotFound("Doctor profile not found.");
+        }
 
         return Ok(doctor);
     }
